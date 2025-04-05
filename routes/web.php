0689e380392146e24c9f1fe10cd8968c692a87db
trisha654 <tasnim.ztrisha@gmail.com>
2025-04-05 21:36:43 +0600

    student added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('dashboard');
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('/student-management', function () {
     return view('student_management');
 });
+
+Route::resource('students', StudentController::class);
